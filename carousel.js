85c3f1335e0f7c5a250afe27c8dbe30f5f6762af
jean-marie <jeanmar-97.1@hotmail.fr>
2022-06-09 13:46:30 +0200

    logique ajouté

--- carousel.js
+++ carousel.js
@@ -1,15 +1,25 @@
 var btn_right = document.querySelector( "#btn_right" );
 var btn_left = document.querySelector( "#btn_left" );
-var init=document.querySelector(".init")
-
-var initialisation = 6;
-
+var init = document.querySelector( ".init" )
+var all_img = document.querySelectorAll( ".img_gif" )
 
 
 
-for (let index = 0; index < initialisation; index++) {
-    $( btn_right ).click( function () {
-        init.style.transform="translate(-200px)"
-    } )
-     
-}
+
+var val = 0
+var count = 0
+
+
+
+$( btn_right ).click( function () {
+    val -= 200
+    init.style.transform = "translate(" + val + "px)"
+    count += 1
+    if ( count == all_img.length ) {
+        val += 200 * all_img.length
+        init.style.transform = "translate(" + val + "px)"
+        count = 0
+    }
+} )
+
+
